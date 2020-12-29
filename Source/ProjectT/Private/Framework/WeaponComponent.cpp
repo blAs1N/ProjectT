8f2a4535ce99b807e2f93a17f313648906f3727a
blAs1N <qazasa123@gmail.com>
2020-12-29 23:08:13 +0900

    Create weapon component

---
+++ Source/ProjectT/Private/Framework/WeaponComponent.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Framework/WeaponComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Engine/World.h"
+#include "Data/WeaponData.h"
+#include "Library/PTStatics.h"
+
+UWeaponComponent::UWeaponComponent()
+{
+	SetIsReplicatedByDefault(true);
+}
+
+void UWeaponComponent::Initialize(const FWeaponData& WeaponData)
+{
+	Stat = WeaponData.Stat;
+	Spread = Stat.MinSpread;
+
+	FPTStatics::AsyncLoad(WeaponData.Mesh, [this, WeaponData]
+	{
+		SetSkeletalMesh(WeaponData.Mesh.Get());
+	});
+
+	SetRelativeTransform(WeaponData.Transform);
+	AttachToComponent(GetAttachParent(), FAttachmentTransformRules::KeepRelativeTransform, WeaponData.Socket);
+}
+
+void UWeaponComponent::TickComponent(float DeltaTime,
+	ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (bFiring)
+	{
+		const auto Delay = 1.0f / (bZooming ? Stat.ZoomSpeed : Stat.Speed);
+		for (; FireLag >= Delay; FireLag -= Delay)
+			Shoot();
+
+		FireLag += DeltaTime;
+		return;
+	}
+	
+	Spread = FMath::Max(Spread - (Stat.SpreadDec * DeltaTime), bZooming ? Stat.ZoomMinSpread : Stat.MinSpread);
+}
+
+void UWeaponComponent::ServerStartFire_Implementation() noexcept
+{
+	FireLag = 1.0f / (bZooming ? Stat.ZoomSpeed : Stat.Speed);
+	bFiring = true;
+}
+
+void UWeaponComponent::Shoot()
+{
+	UE_LOG(LogTemp, Log, TEXT("Shoot!"));
+	Spread = FMath::Max(Spread + Stat.SpreadInc, bZooming ? Stat.ZoomMaxSpread : Stat.MaxSpread);
+}
---
+++ Source/ProjectT/Public/Framework/WeaponComponent.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Data/WeaponStatData.h"
+#include "WeaponComponent.generated.h"
+
+UCLASS()
+class PROJECTT_API UWeaponComponent final : public USkeletalMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UWeaponComponent();
+
+	void Initialize(const struct FWeaponData& InWeaponData);
+
+	FORCEINLINE void StartFire() { ServerStartFire(); }
+	FORCEINLINE void StopFire() { ServerStopFire(); }
+
+	void Reload();
+
+private:
+	void TickComponent(float DeltaTime, ELevelTick TickType,
+		FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerStartFire();
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerStopFire();
+
+	void ServerStartFire_Implementation() noexcept;
+	FORCEINLINE bool ServerStartFire_Validate() const noexcept { return !bFiring; }
+
+	FORCEINLINE void ServerStopFire_Implementation() noexcept { bFiring = false; }
+	FORCEINLINE bool ServerStopFire_Validate() const noexcept { return bFiring; }
+
+	void Shoot();
+
+private:
+	FWeaponStatData Stat;
+	float FireLag;
+	float Spread;
+	uint8 bZooming : 1;
+	uint8 bFiring : 1;
+};

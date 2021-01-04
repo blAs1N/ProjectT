8a14e941f987cebe2d016dfb1b41374f3a38564f
blAs1N <qazasa123@gmail.com>
2021-01-04 23:58:25 +0900

    Create posture data structure

---
+++ Source/ProjectT/Public/Data/PostureData.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "UObject/ObjectMacros.h"
+#include "PostureData.generated.h"
+
+USTRUCT(Atomic, BlueprintType)
+struct PROJECTT_API FPostureStat
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	float SpeedRatio = 1.0f;
+
+	UPROPERTY(EditAnywhere)
+	float HalfHeight;
+
+	UPROPERTY(EditAnywhere)
+	float Radius;
+
+	UPROPERTY(EditAnywhere)
+	float MeshOffset;
+};
+
+
+USTRUCT(Atomic, BlueprintType)
+struct PROJECTT_API FPostureData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	FPostureStat StandData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	FPostureStat CrouchData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	FPostureStat PostureData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	float SprintSpeedRatio;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	float DefaultWalkSpeed;
+};

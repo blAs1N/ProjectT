949e449feaf1f83fa56ce5d6d3a1fa45b0210bd0
blAs1N <qazasa123@gmail.com>
2021-02-01 16:13:50 +0900

    Use static function instead of static class

---
+++ Source/ProjectT/Public/Library/AsyncLoad.h
@@ -0,0 +1,18 @@
+  // Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "UObject/SoftObjectPtr.h"
+
+template <class T, class Func>
+static void AsyncLoad(const TAssetPtr<T>& SoftPtr, Func&& Fn)
+{
+	if (!SoftPtr.IsNull() && SoftPtr.IsPending())
+	{
+		auto Callback = FStreamableDelegate::CreateLambda(Forward<Func>(Fn));
+		UAssetManager::GetStreamableManager().RequestAsyncLoad(SoftPtr.ToSoftObjectPath(), MoveTemp(Callback));
+	}
+	else Fn();
+}

be823ac95105fd1e81890517cf921e98badcdf43
blAs1N <qazasa123@gmail.com>
2021-02-04 00:06:19 +0900

    Implement initialize function

--- Source/ProjectT/Private/Character/PTCharacter.cpp
+++ Source/ProjectT/Private/Character/PTCharacter.cpp
@@ -2,9 +2,56 @@
 
 #include "Character/PTCharacter.h"
 #include "Component/CompositeModelComponent.h"
+#include "Data/CharacterData.h"
+#include "Equipment/Weapon.h"
+#include "Library/AsyncLoad.h"
 
 APTCharacter::APTCharacter(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCompositeModelComponent>(MeshComponentName))
 {
 	PrimaryActorTick.bCanEverTick = true;
+}
+
+void APTCharacter::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	if (UWorld* World = GetWorld())
+	{
+		FActorSpawnParameters Params;
+		Params.Owner = Params.Instigator = this;
+		Weapon = World->SpawnActor<AWeapon>(WeaponClass, Params);
+	}
+
+	Initialize();
+}
+
+#if WITH_EDITOR
+
+void APTCharacter::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	if (!PropertyChangedEvent.Property) return;
+
+	const auto PropertyName = PropertyChangedEvent.GetPropertyName();
+	if (PropertyName == TEXT("DataTable") || PropertyName == TEXT("RowName"))
+		Initialize();
+}
+
+#endif
+
+void APTCharacter::Initialize()
+{
+	static const FCharacterData DefaultData{};
+
+	const auto* Data = CharacterKey.GetRow<FCharacterData>(TEXT(""));
+	if (!Data) Data = &DefaultData;
+
+	GetMesh()->SetRelativeRotation(FRotator{ 0.0f, Data->MeshYaw, 0.0f });
+	GetMesh()->SetAnimClass(Data->AnimClass);
+	AsyncLoad(Data->Mesh, [this, Data]
+		{ GetMesh()->SetSkeletalMesh(Data->Mesh.Get()); });
+
+	Weapon->Initialize(CharacterKey.RowName);
+	Weight = Data->Weight;
 }
--- Source/ProjectT/Public/Character/PTCharacter.h
+++ Source/ProjectT/Public/Character/PTCharacter.h
@@ -3,14 +3,46 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "GameFramework/Character.h"
 #include "PTCharacter.generated.h"
 
 UCLASS(config=Game)
-class APTCharacter : public ACharacter
+class PROJECTT_API APTCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	APTCharacter(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	APTCharacter(const FObjectInitializer& ObjectInitializer);
+
+	float TakeDamage(float Damage, const FDamageEvent& DamageEvent,
+		AController* EventInstigator, AActor* DamageCauser) override;
+
+	FORCEINLINE class AWeapon* GetWeapon() const noexcept { return Weapon; }
+
+private:
+	void PostInitializeComponents() override;
+
+#if WITH_EDITOR
+	void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	void Initialize();
+
+private:
+	UPROPERTY(Transient, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	AWeapon* Weapon;
+
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = true))
+	TSubclassOf<AWeapon> WeaponClass;
+
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = true))
+	FDataTableRowHandle CharacterKey;
+
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = true))
+	float BackConstant;
+
+	float BackPercent;
+
+	float Weight;
 };

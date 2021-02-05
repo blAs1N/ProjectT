90c2dd1dcff74a064a81f6e54bd0858a8e25201b
blAs1N <qazasa123@gmail.com>
2021-02-05 23:15:49 +0900

    Implement take damage

--- Source/ProjectT/Private/Character/PTCharacter.cpp
+++ Source/ProjectT/Private/Character/PTCharacter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Character/PTCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "Component/CompositeModelComponent.h"
 #include "Data/CharacterData.h"
 #include "Equipment/Weapon.h"
@@ -10,6 +11,35 @@ APTCharacter::APTCharacter(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCompositeModelComponent>(MeshComponentName))
 {
 	PrimaryActorTick.bCanEverTick = true;
+}
+
+float APTCharacter::TakeDamage(float Damage, const FDamageEvent&
+	DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	Damage = Super::TakeDamage(Damage, DamageEvent, EventInstigator, DamageCauser);
+	if (Damage <= 0.0f) return 0.0f;
+
+	FVector Dir;
+	if (DamageEvent.IsOfType(FPointDamageEvent::ClassID))
+	{
+		const auto& PointDamageEvent = reinterpret_cast<const FPointDamageEvent&>(DamageEvent);
+		Dir = PointDamageEvent.ShotDirection;
+	}
+	else if (DamageEvent.IsOfType(FRadialDamageEvent::ClassID))
+	{
+		const auto& RadialDamageEvent = reinterpret_cast<const FRadialDamageEvent&>(DamageEvent);
+		Dir = GetActorLocation() - RadialDamageEvent.Origin;
+		Dir.Normalize();
+	}
+
+	float Nnockback = 7.0f * (Damage + 2.0f) * (Damage + BackPercent);
+	Nnockback /= (Weight + 100.0f);
+	Nnockback *= 2.0f;
+	Nnockback += BackConstant;
+
+	GetCharacterMovement()->AddImpulse(Dir * Nnockback, true);
+	BackPercent += Damage;
+	return Damage;
 }
 
 void APTCharacter::PostInitializeComponents()

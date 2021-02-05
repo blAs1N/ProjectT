dd44c6894bcbf7ef7ebbec419e9d2fab6898df62
blAs1N <qazasa123@gmail.com>
2021-02-06 01:33:09 +0900

    Add whitespace

--- Source/ProjectT/Public/Library/AsyncLoad.h
+++ Source/ProjectT/Public/Library/AsyncLoad.h
@@ -11,7 +11,7 @@ static void AsyncLoad(const TSoftObjectPtr<T>& SoftPtr, Func&& Fn)
 {
 	if (!SoftPtr.IsNull() && SoftPtr.IsPending())
 	{
-		auto Callback = FStreamableDelegate::CreateLambda([SoftPtr, Fn = Forward<Func>(Fn)]{ Fn(SoftPtr) });
+		auto Callback = FStreamableDelegate::CreateLambda([SoftPtr, Fn = Forward<Func>(Fn)] { Fn(SoftPtr); });
 		UAssetManager::GetStreamableManager().RequestAsyncLoad(SoftPtr.ToSoftObjectPath(), MoveTemp(Callback));
 	}
 	else Fn(SoftPtr);

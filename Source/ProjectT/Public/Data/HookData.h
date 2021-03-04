8b3cde46c41228a6d90671633a56bc19dbdd85a5
blAs1N <qazasa123@gmail.com>
2021-03-05 00:00:16 +0900

    Set hook data

--- Source/ProjectT/Public/Data/HookData.h
+++ Source/ProjectT/Public/Data/HookData.h
@@ -11,11 +11,35 @@ struct PROJECTT_API FHookData : public FTableRowBase
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TSoftObjectPtr<class UMaterialInterface> Material;
+	TSoftObjectPtr<class UStaticMesh> HookMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSoftObjectPtr<class UMaterialInterface> CableMaterial;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSoftObjectPtr<class USoundBase> ThrowSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSoftObjectPtr<USoundBase> PullSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FTransform HookTransform;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	FName HandSocket;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float Speed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	float Distance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float Thickness;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float MaxMoveDuration;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float PenetrationOffset;
 };

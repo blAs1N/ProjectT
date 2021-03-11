d6928e8be81a6a1e465b72c4fd6edaf289798578
blAs1N <qazasa123@gmail.com>
2021-03-12 04:59:04 +0900

    Use SetCollision

--- Source/ProjectT/Private/Equipment/State/MoveState.cpp
+++ Source/ProjectT/Private/Equipment/State/MoveState.cpp
@@ -1,16 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Equipment/State/MoveState.h"
+#include "Components/CapsuleComponent.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Equipment/Hook.h"
-#include "Kismet/KismetSystemLibrary.h"
 
 void FMoveState::Enter()
 {
-	const auto Target = GetOwner<AHook>()->GetOwner<ACharacter>();
-	
+	const auto Hook = GetOwner<AHook>();
+	Hook->SetCollision(false);
+
+	const auto Target = Hook->GetOwner<ACharacter>();
 	StartLoc = Target->GetActorLocation();
+
 	Target->GetCharacterMovement()->
 		SetMovementMode(EMovementMode::MOVE_Flying);
 }
@@ -37,36 +40,7 @@ void FMoveState::Tick(float DeltaSeconds)
 	TimeElapsed += DeltaSeconds;
 
 	const float BlendPct = MaxMoveDuration > 0.0f ? TimeElapsed / MaxMoveDuration : 1.0f;
-	FVector NewLoc = bComplete ? HookLoc : FMath::Lerp(StartLoc, HookLoc, BlendPct);
-	
-	FCollisionQueryParams Param;
-	Param.AddIgnoredActor(Hook);
-	Param.AddIgnoredActor(Target);
-
-	const FVector MeshLoc = Target->GetMesh()->GetComponentLocation();
-	const FVector DiffZ = FVector{ 0.0f, 0.0f, Target->GetActorLocation().Z - MeshLoc.Z };
-	const auto World = Hook->GetWorld();
-
-	TArray<AActor*> Ignores{ Hook, Target };
-
-	FHitResult Result;
-	const bool bHit = UKismetSystemLibrary::LineTraceSingle(World, MeshLoc, NewLoc - DiffZ,
-		ETraceTypeQuery::TraceTypeQuery1, false, Ignores, EDrawDebugTrace::None, Result, false);
-	/*const bool bHit = World->LineTraceSingleByChannel(Result,
-		MeshLoc, NewLoc - DiffZ, ECollisionChannel::ECC_Visibility, Param);*/
-
-	if (bHit)
-	{
-		UKismetSystemLibrary::LineTraceSingle(World, NewLoc - DiffZ, NewLoc - DiffZ + FVector{ 0.0f, 0.0f, 100.0f },
-			ETraceTypeQuery::TraceTypeQuery1, false, Ignores, EDrawDebugTrace::ForDuration, Result, false);
-
-		World->LineTraceSingleByChannel(Result, NewLoc - DiffZ, NewLoc - DiffZ +
-			FVector{ 0.0f, 0.0f, 100.0f }, ECollisionChannel::ECC_Visibility, Param);
-
-		
-		NewLoc = Result.Location + DiffZ;
-	}
-
+	const FVector NewLoc = bComplete ? HookLoc : FMath::Lerp(StartLoc, HookLoc, BlendPct);
 	Target->SetActorLocation(NewLoc);
 }
 
@@ -74,6 +48,8 @@ void FMoveState::Exit()
 {
 	const auto Hook = GetOwner<AHook>();
 	const auto Target = Hook->GetOwner<ACharacter>();
+
+	Hook->SetCollision(true);
 	Target->GetCharacterMovement()->SetDefaultMovementMode();
 
 	const FVector Dir = Target->GetActorLocation() - StartLoc;

ebef39ce7e365289b1b5f97bb432da5fdd0717be
blAs1N <qazasa123@gmail.com>
2021-03-14 22:49:42 +0900

    Apply skill in weapon data

--- Source/ProjectT/Public/Data/WeaponData.h
+++ Source/ProjectT/Public/Data/WeaponData.h
@@ -2,8 +2,19 @@
 
 #include "Engine/DataTable.h"
 #include "UObject/SoftObjectPtr.h"
-#include "Data/WeaponStat.h"
 #include "WeaponData.generated.h"
+
+USTRUCT(Atomic, BlueprintType)
+struct FSkillData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<class USkill> Class;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSoftObjectPtr<class UDataAsset> Data;
+};
 
 USTRUCT(Atomic, BlueprintType)
 struct PROJECTT_API FWeaponData : public FTableRowBase
@@ -11,17 +22,17 @@ struct PROJECTT_API FWeaponData : public FTableRowBase
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TSoftObjectPtr<class USkeletalMesh> Mesh;
+	TArray<FSkillData> Skills;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TSubclassOf<class UAnimInstance> AnimClass;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	FName Socket;
+	TSoftObjectPtr<class UStaticMesh> Mesh;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	FTransform Transform;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	FWeaponStat Stat;
+	FName HandSocket;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FName MuzzleSocket;
 };

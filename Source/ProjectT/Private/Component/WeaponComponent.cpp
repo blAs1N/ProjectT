620b469850c94616dba76655c932197fd24c0664
blAs1N <qazasa123@gmail.com>
2021-03-14 22:53:32 +0900

    Apply weapon in weapon component

--- Source/ProjectT/Private/Component/WeaponComponent.cpp
+++ Source/ProjectT/Private/Component/WeaponComponent.cpp
@@ -2,17 +2,107 @@
 
 #include "Component/WeaponComponent.h"
 #include "Engine/DataTable.h"
+#include "Net/UnrealNetwork.h"
 #include "Character/PTCharacter.h"
 #include "Data/WeaponData.h"
+#include "Equipment/Weapon.h"
 #include "MISC/DataTableLoader.h"
 
 UWeaponComponent::UWeaponComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 	SetIsReplicatedByDefault(true);
+
+	WeaponClass = AWeapon::StaticClass();
+}
+
+void UWeaponComponent::BeginSkill(uint8 Index)
+{
+	check(WeaponInst);
+	WeaponInst->BeginSkill(Index);
+
+	ServerBeginSkill(Index);
+}
+
+void UWeaponComponent::EndSkill(uint8 Index)
+{
+	check(WeaponInst);
+	WeaponInst->EndSkill(Index);
+
+	ServerEndSkill(Index);
+}
+
+void UWeaponComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	if (!GetOwner()->HasAuthority())
+		return;
+
+	FActorSpawnParameters Param;
+	Param.Owner = GetOwner();
+
+	WeaponInst = GetWorld()->SpawnActor<AWeapon>(WeaponClass, Param);
+	OnRep_Weapon();
 }
 
 void UWeaponComponent::OnInitialize(int32 InKey)
 {
 	Key = InKey;
+	OnRep_Weapon();
+}
+
+void UWeaponComponent::OnGetData(const FWeaponData& Data)
+{
+	const auto OnLoadObject = [this, Data](const auto& Ptr)
+	{
+		const auto* Owner = GetOwner<APTCharacter>();
+		const auto WeaponMesh = Owner->GetWeaponMeshComponent();
+
+		WeaponMesh->SetStaticMesh(Ptr.Get());
+		WeaponMesh->SetRelativeTransform(Data.Transform);
+		WeaponMesh->AttachToComponent(Owner->GetMesh(),
+			FAttachmentTransformRules::KeepRelativeTransform, Data.HandSocket);
+
+		if (WeaponInst)
+		{
+			WeaponInst->AttachToComponent(WeaponMesh,
+				FAttachmentTransformRules::SnapToTargetIncludingScale, Data.MuzzleSocket);
+		}
+	};
+
+	const bool bLoadAsync = IInitializable::Execute_IsLoadAsync(GetOwner());
+	LoadObject(Data.Mesh, OnLoadObject, bLoadAsync);
+}
+
+void UWeaponComponent::GetLifetimeReplicatedProps(
+	TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(UWeaponComponent, WeaponInst);
+}
+
+void UWeaponComponent::OnRep_Weapon()
+{
+	if (!Key) return;
+
+	const bool bLoadAsync = IInitializable::Execute_IsLoadAsync(GetOwner());
+	GetData<FWeaponData>(WeaponDataTable, Key, [this, bLoadAsync](const auto& Data)
+		{
+			OnGetData(Data);
+
+			if (WeaponInst)
+				WeaponInst->Initialize(Data, bLoadAsync);
+		}, bLoadAsync);
+}
+
+void UWeaponComponent::ServerBeginSkill_Implementation(uint8 Index)
+{
+	if (!GetOwner()->HasLocalNetOwner())
+		WeaponInst->BeginSkill(Index);
+}
+
+void UWeaponComponent::ServerEndSkill_Implementation(uint8 Index)
+{
+	if (!GetOwner()->HasLocalNetOwner())
+		WeaponInst->EndSkill(Index);
 }
--- Source/ProjectT/Public/Component/WeaponComponent.h
+++ Source/ProjectT/Public/Component/WeaponComponent.h
@@ -15,12 +15,42 @@ class PROJECTT_API UWeaponComponent final : public UActorComponent, public IInit
 public:	
 	UWeaponComponent();
 
+	UFUNCTION(BlueprintCallable)
+	void BeginSkill(uint8 Index);
+
+	UFUNCTION(BlueprintCallable)
+	void EndSkill(uint8 Index);
+
 private:
+	void BeginPlay() override;
+
 	void OnInitialize(int32 InKey) override;
+	void OnGetData(const struct FWeaponData& Data);
+
+	void GetLifetimeReplicatedProps(TArray
+		<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UFUNCTION()
+	void OnRep_Weapon();
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerBeginSkill(uint8 Index);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerEndSkill(uint8 Index);
+
+	FORCEINLINE bool ServerBeginSkill_Validate(uint8 Index) const noexcept { return true; }
+	FORCEINLINE bool ServerEndSkill_Validate(uint8 Index) const noexcept { return true; }
 
 private:
 	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = true))
 	TSoftObjectPtr<class UDataTable> WeaponDataTable;
+
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	TSubclassOf<class AWeapon> WeaponClass;
+
+	UPROPERTY(Transient, ReplicatedUsing = OnRep_Weapon)
+	AWeapon* WeaponInst;
 
 	int32 Key;
 };
